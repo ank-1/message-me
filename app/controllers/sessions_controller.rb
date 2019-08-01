2f85fcef5def944791f1e37fbbeba49f6233f85f
ank-1 <kaushik.dharmesh97@gmail.com>
2019-08-02 03:38:02 +0530

    completed authentication system for users and added global controller methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,23 @@
 class SessionsController < ApplicationController
   def new
-
+     @user=User.new
   end
 
   def create
-
+    user=User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      flash.now[:success]= "You have successfully logged in."
+      session[:user_id]=user.id
+      redirect_to root_path
+    else
+      flash[:error]="Incorrect credentials! Try again"
+      render 'new'
+    end
   end
 
   def destroy
-
+    session[:user_id]=nil
+    flash[:success]="You have successfully logged out"
+    redirect_to login_path
   end
 end
