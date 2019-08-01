aa116cadca4e966a6f4101ab732661e71f31a18b
ank-1 <kaushik.dharmesh97@gmail.com>
2019-08-02 00:42:16 +0530

    added message model and association b/w user and messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :messages
   validates :username,presence: true,uniqueness: true,
             uniqueness: { case_sensitive: false },
             length:{minimum:4,maximum:15}
